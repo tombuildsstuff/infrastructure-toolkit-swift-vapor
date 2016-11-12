4c5c88e277103ef983d441dbd400441cde464849
tombuildsstuff <git@ibuildstuff.co.uk>
2016-11-12 15:02:46 +0000

    Upgrading to Swift 3.0.1

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "InfrastructureToolkitVapor",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 0),
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 1),
         .Package(url: "git@github.com:tombuildsstuff/infrastructure-toolkit-swift.git", majorVersion: 0, minor: 1)
     ]
 )
